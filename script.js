b339a8aa515e6075be07ef6a501f605fb7e292b2
Volodya <valadimir.khoruzhik@mail.ru>
2024-10-02 21:13:00 +0500

    final version

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let btn = document.querySelector(".button");
+let btn = document.querySelector("#start");
 btn.addEventListener('click', (event) =>{
     start();
 })
+let reset = document.querySelector("#reset");
+reset.addEventListener('click', (event) =>{
+    deleteItems();
+})
+
 
 function start(){
     alert('Enter mesh parameters(max:100x100)');
@@ -15,8 +20,17 @@ function start(){
         for(let j = widthMesh; j > 0; j--){
             const divMesh = document.createElement("div");
             div.appendChild(divMesh);
+            divMesh.addEventListener('mouseover', (event) => {
+                event.target.classList.add('active'),{ once: true }});
+
             divMesh.classList.add("divMesh");
         }
     }
 }
 
+function deleteItems() {
+    let deleteElement = document.querySelectorAll('.rowContainer');
+    for (let i = 0; i < deleteElement.length; i++) {
+      deleteElement[i].remove();
+    }
+  }
